92c691a0dc3e26e77c49053333f377d97942d262
TsayemDaniela <tsadaniela@gmail.com>
2021-03-14 18:04:57 +0100

    def N4

---
+++ src/algorithms.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def N4(p, a, b):
+    N = []
+    if p[1] != 0: N.append((p[0], p[1]-1))
+    if p[0] != 0: N.append((p[0]-1, p[1]))
+    if p[1] != b-1: N.append((p[0], p[1]+1))
+    if p[0] != a-1: N.append((p[0]+1, p[1]))
+    return N
