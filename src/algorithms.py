5e1b3473859e1e10a444084ca6782eb1ac0c94d5
TsayemDaniela <tsadaniela@gmail.com>
2021-03-15 23:41:09 +0100

    final changes (hopefully)

--- src/algorithms.py
+++ src/algorithms.py
@@ -3,30 +3,46 @@ import numpy as np
 # Function for 4 Pixel Neighbourhood
 def N4(p, a, b):
     N = []
-    if p[1] != 0: N.append((p[0], p[1]-1))
-    if p[0] != 0: N.append((p[0]-1, p[1]))
-    if p[1] != b-1: N.append((p[0], p[1]+1))
-    if p[0] != a-1: N.append((p[0]+1, p[1]))
+    if p[1] != 0: 
+        N.append((p[0], p[1]-1))
+    if p[0] != 0: 
+        N.append((p[0]-1, p[1]))
+    if p[1] != b-1: 
+        N.append((p[0], p[1]+1))
+    if p[0] != a-1: 
+        N.append((p[0]+1, p[1]))
     return N
 
 # Function for 8 Pixel Neighbourhood
 def N8(p, a, b):
     N = []
-    if p[1] != 0: N.append((p[0], p[1]-1))
-        if p[0] != 0: N.append((p[0]-1, p[1]-1))
-        if p[0] != a-1: N.append((p[0]+1, p[1])-1)
+    if p[1] != 0: 
+        N.append((p[0], p[1]-1))
+        if p[0] != 0: 
+            N.append((p[0]-1, p[1]-1))
+        if p[0] != a-1: 
+            N.append((p[0]+1, p[1]-1))
 
-    if p[0] != 0: N.append((p[0]-1, p[1]))
-        if p[1] != 0: N.append((p[0]-1, p[1]-1))
-        if p[1] != b-1: N.append((p[0]-1, p[1]+1))
+    if p[0] != 0: 
+        N.append((p[0]-1, p[1]))
+        if p[1] != 0: 
+            N.append((p[0]-1, p[1]-1))
+        if p[1] != b-1: 
+            N.append((p[0]-1, p[1]+1))
 
-    if p[1] != b-1: N.append((p[0], p[1]+1))
-        if p[0] != 0: N.append((p[0]-1, p[1]+1))
-        if p[0] != a-1: N.append((p[0]+1, p[1]+1))
+    if p[1] != b-1: 
+        N.append((p[0], p[1]+1))
+        if p[0] != 0: 
+            N.append((p[0]-1, p[1]+1))
+        if p[0] != a-1: 
+            N.append((p[0]+1, p[1]+1))
 
-    if p[0] != a-1: N.append((p[0]+1, p[1]))
-        if p[1] != 0: N.append((p[0]+1, p[1]-1))
-        if p[1] != b-1: N.append((p[0]+1, p[1]+1))
+    if p[0] != a-1: 
+        N.append((p[0]+1, p[1]))
+        if p[1] != 0: 
+            N.append((p[0]+1, p[1]-1))
+        if p[1] != b-1: 
+            N.append((p[0]+1, p[1]+1))
     return N
 
 def WS(inp, Nx):
--- src/ensemble.py
+++ src/ensemble.py
@@ -1,7 +1,24 @@
 import cv2
+import sys
 from PIL import Image
 import numpy as np
-from function import N4, N8, WS
+from algorithms import N4, N8, WS
+
+#for f1 and f2
+#load input
+ipt = np.loadtxt(open("../input/f1_dinv.txt"), delimiter = ",")
+opt_N4 = WS(ipt, N4)
+opt_N8 = WS(ipt, N8)
+np.savetxt("../output/f1_dinv_wt_4.txt", opt_N4, fmt = '%d', delimiter = ',', newline = '\n')
+np.savetxt("../output/f1_dinv_wt_8.txt", opt_N8, fmt = '%d', delimiter = ',', newline = '\n')
+
+ipt = np.loadtxt(open("../input/f2.txt"), delimiter = ",")
+opt_N4 = WS(ipt, N4)
+opt_N8 = WS(ipt, N8)
+np.savetxt("../output/f2_wt_4.txt", opt_N4, fmt = '%d', delimiter = ',', newline = '\n')
+np.savetxt("../output/f2_wt_8.txt", opt_N8, fmt = '%d', delimiter = ',', newline = '\n')
+
+#for 3 self images
 
 ipt1 = cv2.imread("../input/f3.png", 0)
 ipt1 = cv2.medianBlur(ipt1, 5)
@@ -14,25 +31,25 @@ ipt2 = cv2.medianBlur(ipt2, 23)
 ipt2 = cv2.bitwise_not(ipt2)
 
 ipt3 = cv2.imread("../input/f5.png", 0)
-ipt3 = cv2.medianBlur(ipt2, 5)  
-ipt3 = cv2.adaptiveThreshold(ipt2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 3)
+ipt3 = cv2.medianBlur(ipt3, 5)  
+ipt3 = cv2.adaptiveThreshold(ipt3, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 3)
 
-def to_im(out, name):
-        im = Image.fromarray(out)
-        cv2.imwrite(name, out)
+def arr_to_im(opt, name):
+        im = Image.fromarray(opt)
+        cv2.imwrite(name, opt)
 
-out1 = watershed(ipt1, neighbor_8)
-out2 = watershed(ipt2, neighbor_8)
-out3 = watershed(ipt3, neighbor_8)
+opt1 = WS(ipt1, N8)
+opt2 = WS(ipt2, N8)
+opt3 = WS(ipt3, N8)
 
-to_im(inp1, "../output/f3_seg.png")
-to_im(out1, "../output/f3_wt_8.png")
-np.savetxt("../output/f3_wt_8.txt", inp, fmt='%d', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
+arr_to_im(ipt1, "../output/f3_seg.png")
+arr_to_im(opt1, "../output/f3_wt_8.png")
+np.savetxt("../output/f3_wt_8.txt", ipt1, fmt='%d', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
 
-to_im(inp2, "../output/f4_seg.png")
-to_im(out2, "../output/f4_wt_8.png")
-np.savetxt("../output/f4_wt_8.txt", inp, fmt='%d', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
+arr_to_im(ipt2, "../output/f4_seg.png")
+arr_to_im(opt2, "../output/f4_wt_8.png")
+np.savetxt("../output/f4_wt_8.txt", ipt2, fmt='%d', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
 
-to_im(inp3, "../output/f5_seg.png")
-to_im(out3, "../output/f5_wt_8.png")
-np.savetxt("../output/f5_wt_8.txt", inp, fmt='%d', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
+arr_to_im(ipt3, "../output/f5_seg.png")
+arr_to_im(opt3, "../output/f5_wt_8.png")
+np.savetxt("../output/f5_wt_8.txt", ipt3, fmt='%d', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
