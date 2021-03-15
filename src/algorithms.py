d9b4c96c302c54e76e07eae1734ce63436a0266f
TsayemDaniela <tsadaniela@gmail.com>
2021-03-15 11:23:45 +0100

    def N8

--- src/algorithms.py
+++ src/algorithms.py
@@ -7,3 +7,23 @@ def N4(p, a, b):
     if p[1] != b-1: N.append((p[0], p[1]+1))
     if p[0] != a-1: N.append((p[0]+1, p[1]))
     return N
+
+def N8(p, a, b):
+    N = []
+    if p[1] != 0: N.append((p[0], p[1]-1))
+        if p[0] != 0: N.append((p[0]-1, p[1]-1))
+        if p[0] != a-1: N.append((p[0]+1, p[1])-1)
+
+    if p[0] != 0: N.append((p[0]-1, p[1]))
+        if p[1] != 0: N.append((p[0]-1, p[1]-1))
+        if p[1] != b-1: N.append((p[0]-1, p[1]+1))
+
+
+    if p[1] != b-1: N.append((p[0], p[1]+1))
+        if p[0] != 0: N.append((p[0]-1, p[1]+1))
+        if p[0] != a-1: N.append((p[0]+1, p[1]+1))
+
+    if p[0] != a-1: N.append((p[0]+1, p[1]))
+        if p[1] != 0: N.append((p[0]+1, p[1]-1))
+        if p[1] != b-1: N.append((p[0]+1, p[1]+1))
+    return N
